1baa66a4385506dcfc59b8dada1593d0395b11c7
Junior Neves <junior.neves@outlook.com>
2021-09-10 23:30:40 -0300

    Add Pet and Owners migration

---
+++ database/migrations/2021_09_11_021823_create_pets_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("name");
+            $table->integer("age");
+            $table->tinyInteger("species");
+            $table->string("breed");
+            $table->unsignedBigInteger("owner_id");
+            $table->softDeletes();
+
+            $table->foreign('owner_id')->references('id')->on('owners');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pets');
+    }
+}
