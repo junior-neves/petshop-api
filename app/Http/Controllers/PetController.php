da339918461f08aac980970ff835589bab85f1a5
Junior Neves <junior.neves@outlook.com>
2021-09-11 00:23:24 -0300

    Add Pet Model and Controller

---
+++ app/Http/Controllers/PetController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pet;
+use Illuminate\Http\Request;
+
+class PetController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->class = Pet::class;
+    }
+
+}
---
+++ app/Models/Pet.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Pet extends Model
+{
+    use SoftDeletes;
+
+    const SPECIES_DOG = 1;
+    const SPECIES_CAT = 2;
+
+    public $timestamps = false;
+    protected $hidden = ['deleted_at'];
+    protected $fillable = [
+        'name', 'age', 'species', 'breed', 'owner'
+    ];
+    protected $casts = [
+        'age' => 'integer',
+        'species' => 'integer'
+    ];
+
+    public function owner()
+    {
+        return $this->belongsTo(Owner::class);
+    }
+
+}
