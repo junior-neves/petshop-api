049220e44b216bb492f9700cf13cc5e24cab5e31
Junior Neves <junior.neves@outlook.com>
2021-09-11 00:22:13 -0300

    Add base functions to the main controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,64 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
-class Controller extends BaseController
+abstract class Controller extends BaseController
 {
-    //
+    protected string $class;
+
+    public function index() : Response
+    {
+        return Response($this->class::all(), 200);
+    }
+
+
+    public function store(Request $request) : Response
+    {
+        $owner = $this->class::create([
+            'name' => $request->name,
+            'phone' => $request->phone,
+
+        ]);
+
+        return Response($owner,200);
+    }
+
+
+    public function show($id) : Response
+    {
+        $owner = $this->class::find($id);
+        if (!$owner) {
+            return Response([],404);
+        }
+
+        return Response([$owner],200);
+    }
+
+
+    public function update(Request $request, $id) : Response
+    {
+        $owner = $this->class::find($id);
+
+        if (!$owner) {
+            return Response([],404);
+        }
+
+        $owner->fill($request->all());
+        $owner->save();
+
+        return Response([$owner],200);
+    }
+
+
+    public function destroy($id) : Response
+    {
+        $owner = $this->class::destroy($id);
+        if (!$owner) {
+            return Response([],404);
+        }
+
+        return Response([],200);
+    }
 }
