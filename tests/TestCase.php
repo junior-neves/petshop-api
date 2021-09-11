7c11bf62bfe0b0990b9d3b9882209967e37e7596
Junior Neves <junior.neves@outlook.com>
2021-09-11 14:08:29 -0300

    Config PHPUnit and TestCase class

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,9 @@ use Laravel\Lumen\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    // The base URL to use while testing the application.
+    protected $baseUrl = 'http://petshop-api.localhost';
+
     /**
      * Creates the application.
      *
