4299aa9f21d1ae3f04cced8d48fa1f58b1ebc24c
Junior Neves <junior.neves@outlook.com>
2021-10-01 18:19:39 -0300

    Add Services, Employees and Schedules routes

--- routes/web.php
+++ routes/web.php
@@ -26,3 +26,34 @@ $router->group(['prefix' => 'pets'], function () use ($router) {
     $router->put('{id}',    'PetController@update');
     $router->delete('{id}', 'PetController@destroy');
 });
+
+$router->group(['prefix' => 'employees'], function () use ($router) {
+    $router->post('',       'EmployeeController@store');
+    $router->get('',        'EmployeeController@index');
+    $router->get('{id}',    'EmployeeController@show');
+    $router->put('{id}',    'EmployeeController@update');
+    $router->delete('{id}', 'EmployeeController@destroy');
+
+    $router->post('{id}/services',             'EmployeeController@storeService');
+    $router->get('{id}/services',              'EmployeeController@indexService');
+    $router->delete('{id}/services/{service}', 'EmployeeController@destroyService');
+
+    $router->get('{id}/schedule/{date}',                     'EmployeeController@showSchedule');
+    $router->get('{id}/schedule/{date}/available/{service}', 'EmployeeController@showScheduleAvailable');
+});
+
+$router->group(['prefix' => 'services'], function () use ($router) {
+    $router->post('',       'ServiceController@store');
+    $router->get('',        'ServiceController@index');
+    $router->get('{id}',    'ServiceController@show');
+    $router->put('{id}',    'ServiceController@update');
+    $router->delete('{id}', 'ServiceController@destroy');
+});
+
+$router->group(['schedules' => 'schedules'], function () use ($router) {
+    $router->post('',       'ScheduleController@store');
+    $router->get('',        'ScheduleController@index');
+    $router->get('{id}',    'ScheduleController@show');
+    $router->put('{id}',    'ScheduleController@update');
+    $router->delete('{id}', 'ScheduleController@destroy');
+});
